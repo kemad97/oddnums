61639b407599dc3fddd845938dc81e254bba95ac
kerolos <kemad99977@gmail.com>
2024-09-28 16:55:16 +0300

    initial commit

---
+++ oddEvenNumsAPP/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+    namespace OddEvenNmsAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            Console.Write("sss");
+        }
+    }
+}
